cbb5817094083853063e61696c3afe567f531c08
Alejandro Aguirre <alejandro.ag@avangenio.com>
2024-08-02 11:11:26 -0400

    Link to Gitlab Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,14 @@ export default function Home() {
   return (
     <main className="relative grid  grid-cols-1 slg:grid-cols-2 gap-12  px-4 py-12 sm:py-16 sm:px-8 md:px-10 slg:p-16 lg:p-24">
       <div className="col-span-full group w-full flx flex-col items-center justify-center space-y-2 sm:space-y-4 mb-4 text-center">
-        <Link href="https://github.com" target="_blank">
+        <Link
+          href="https://gitlab.generalsoftwareinc.com/alejandro.ag/bio-generator-by-ai.git"
+          target="_blank"
+        >
           <AnimatedGradientText className="px-6 py-2">
             <Star className="w-6 h-6 fill-yellow-300 text-yellow-400" />
             <hr className="mx-2 h-4 w-[1px] bg-gray-300" />
-            Star on Github
+            Star on Gitlab
             <ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
           </AnimatedGradientText>
         </Link>
